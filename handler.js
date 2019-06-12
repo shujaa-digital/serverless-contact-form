26ec01e71b4da94a8584e46f0ee20a6278979912
trev-of-ev <tdguinn87@gmail.com>
2019-06-12 18:50:47 -0500

    First Commit

---
+++ handler.js
@@ -0,0 +1,61 @@
+const serverless = require("serverless-http");
+const AWS = require("aws-sdk");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+if (!AWS.config.region) {
+  AWS.config.update({
+    region: "us-east-1" //example us-east-1
+  });
+}
+
+const ses = new AWS.SES();
+
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const message = req.body.message;
+
+  const emailParams = {
+    Source: "info@shujaadigital.com", // Your Verified Email
+    Destination: {
+      ToAddresses: ["info@shujaadigital.com"] // Your verfied Email
+    },
+    ReplyToAddresses: [req.body.email],
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: `${message}  from  ${req.body.email}`
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "You Received a Message from www.shujaadigital.com"
+      }
+    }
+  };
+
+  ses.sendEmail(emailParams, (err, data) => {
+    if (err) {
+      res.status(402).send(`${err} ${err.stack}`);
+    }
+    if (data) {
+      res.send(data);
+    }
+  });
+});
+
+module.exports.form = serverless(app);
